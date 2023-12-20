35c360cdfe9848557c017086a21d0fa2066ae543
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-12-20 00:29:32 -0600

    Update main.js

--- flag-search/main.js
+++ flag-search/main.js
@@ -11,21 +11,39 @@ searchBtn.addEventListener("click", () => {
         class="flag-img">
         <h2>${data[0].name.common}</h2>
         <div class="wrapper">
-        <div class="data-wrapper">
-        <h4>Capital:</h4>
-        <span>${data[0].capital[0]}</span>
-        </div>
-        <h2>${data[0].name.common}</h2>
-        <div class="wrapper">
-        <div class="data-wrapper">
-        <h4>Continent:</h4>
-        <span>${data[0].continents[0]}</span>
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
         </div>
         <div class="wrapper">
-        <div class="data-wrapper">
-        <h4>Population:</h4>
-        <span>${data[0].population[0]}</span>
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
         </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Population:</h4>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${
+                  data[0].currencies[Object.keys(data[0].currencies)]
+                } - ${Object.keys(data[0].currencies)[0]}</span>
+            </div>
+        </div>
+         <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Languages:</h4>
+                <span>${Object.values(data[0].languages)
+                  .toString()
+                  .split(",")
+                  .join(", ")}</span>
+            </div>
         </div>
         `
     });
